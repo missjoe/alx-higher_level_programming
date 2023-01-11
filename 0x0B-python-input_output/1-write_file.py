9f4a4297b8b9b0dc6c56910eb8f3691049b59b27
John Tochi Josephine <johntochijosephine1995@gmail.com>
2023-01-11 08:37:11 -0800

    File i/o

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -3,7 +3,7 @@
 
 
 def write_file(filename="", text=""):
-        """Writes a string to a UTF8 text file
-            """
-                with open(filename, "w", encoding="utf-8") as f:
-                            return f.write(text)
+    """Writes a string to a UTF8 text file
+    """
+    with open(filename, "w", encoding="utf-8") as f:
+        return f.write(text)
