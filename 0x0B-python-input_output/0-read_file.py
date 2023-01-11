6c8a6c0a3d8a55a440aaf8a895d85bd376309642
John Tochi Josephine <johntochijosephine1995@gmail.com>
2023-01-11 08:19:00 -0800

    Task 0

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -3,6 +3,6 @@
 
 
 def read_file(filename=""):
-        """Prints the contents of a UTF8 text file"""
-            with open(filename, encoding="utf-8") as f:
-                        print(f.read(), end="")
+    """Prints the contents of a UTF8 text file"""
+    with open(filename, encoding="utf-8") as f:
+        print(f.read(), end="")
