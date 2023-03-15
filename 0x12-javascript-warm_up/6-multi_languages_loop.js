f6e5c4a5b7b78a61a0fd2fd7bb040a5127a8d769
John Tochi Josephine <johntochijosephine1995@gmail.com>
2023-03-15 11:37:57 -0700

    Task 6

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,5 +1,6 @@
 #!/usr/bin/node
-let words = ['C is fun', 'Python is cool', 'Javascript is amazing'];
+
+let words = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
 for (let i = 0; i < words.length; i++) {
   console.log(words[i]);
 }
