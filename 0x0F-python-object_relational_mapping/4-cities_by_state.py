2028f90f548efd972db588fd8d224be5960fba69
John Tochi Josephine <johntochijosephine1995@gmail.com>
2023-03-20 03:43:55 -0700

    Task 4

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -1,25 +1,37 @@
 #!/usr/bin/python3
-# gets all CITIES
+"""
+This script lists all cities from
+the database `hbtn_0e_4_usa`.
+"""
 
+import MySQLdb
+from sys import argv
 
-def main(args):
-    # gets all CITY stuff
-    if len(args) != 4:
-        raise Exception("need 3 arguments!")
-    db = MySQLdb.connect(host='localhost',
-                         user=args[1],
-                         passwd=args[2],
-                         db=args[3])
-    cur = db.cursor()
-    cur.execute("SELECT c.id,\
-                c.name, s.name FROM cities c\
-                JOIN states s ON s.id=c.state_id ORDER BY c.id")
-    states = cur.fetchall()
-    for state in states:
-        print(state)
+if __name__ == '__main__':
+    """
+    Access to the database and get the cities
+    from the database.
+    """
 
+    db = MySQLdb.connect(host="localhost", user=argv[1], port=3306,
+                         passwd=argv[2], db=argv[3])
 
-if __name__ == "__main__":
-    import sys
-    import MySQLdb
-    main(sys.argv)
+    with db.cursor() as cur:
+        cur.execute("""
+            SELECT
+                cities.id, cities.name, states.name
+            FROM
+                cities
+            JOIN
+                states
+            ON
+                cities.state_id = states.id
+            ORDER BY
+                cities.id ASC
+        """)
+
+        rows = cur.fetchall()
+
+    if rows is not None:
+        for row in rows:
+            print(row)
